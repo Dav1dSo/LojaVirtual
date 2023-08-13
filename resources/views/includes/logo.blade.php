6797677a51f728aab794378713ced5a58bcd6ba7
portugues-2020 <Dav1dso>
2023-08-13 11:30:14 -0300

    Refatored template Login.

---
+++ resources/views/includes/logo.blade.php
@@ -0,0 +1,1 @@
+<a class="navbar-brand" href="/">FULL<strong class="text-info">Ecommerce</strong></a>
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,13 +1,13 @@
 <div>
   <nav class="navbar p-4 navbar-dark bg-dark fixed-top">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">FULL<strong class="text-info">Ecommerce</strong></a>
+        @include('includes.logo')
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">FULL<strong class="text-info">Ecommerce</strong></h5>
+          @include('includes.logo')
             <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
