53e49bb19dfcf0495c3421bef1d8c938cd726685
David <soaresdavid411@gmail.com>
2023-08-14 08:48:18 -0300

    Add message error.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,11 +27,21 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="mb-3 w-50 mx-auto">
-                    <label for="email" class="form-label">Email</label>
+                    <div class="d-flex justify-content-between">
+                        <label for="email" class="form-label">Email</label>
+                        @error('email')
+                            <p class="font-weight-light text-danger text-center">Email não encontrado!</p>
+                        @enderror
+                    </div>                    
                     <input name="email" type="email" class="form-control" id="email" required autofocus autocomplete="username">
                 </div>
                 <div class="mb-3 w-50 mx-auto">
-                    <label for="password" class="form-label">Password</label>
+                    <div class="d-flex justify-content-between">
+                        <label for="password" class="form-label">Password</label>
+                        @error('email')
+                            <p class="font-weight-light text-danger text-center">Senha incorreta!</p>
+                        @enderror
+                    </div>
                     <input name="password" type="password" class="form-control" id="password" required autocomplete="current-password">
                 </div>
                 <div class="mb-3 w-50 mx-auto d-flex justify-content-between">
@@ -54,50 +64,3 @@
 @include('includes.scriptsbootstrap')
     
 </html>
-
-{{-- 
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>  --}}
