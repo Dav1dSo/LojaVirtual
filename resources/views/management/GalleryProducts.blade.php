fac2680c391834e415666c53c700130143244a97
David <soaresdavid411@gmail.com>
2023-10-08 11:18:32 -0300

    Feature: updated style of images.

--- resources/views/management/GalleryProducts.blade.php
+++ resources/views/management/GalleryProducts.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
     <head>
         <title>Gallery Products</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -12,34 +14,56 @@
     #wrap {
         margin-top: 100px;
     }
+
+    .imgProduct {
+        height: 20rem;
+    }
 </style>
 
 <body>
     @include('includes.navbar')
-    
-    
-    <div class="row row-cols-1 row-cols-md-4 p-5" id="wrap">
 
+
+    <div class="row row-cols-1 row-cols-md-4 p-5" id="wrap">
         @foreach ($gallery as $imageProduct)
-        <div class="col-md-3 mb-5">
-            <div class="card h-100">
-                <img src="{{ url("storage/$imageProduct->imagem") }}" class="card-img-top h-75" alt="...">
-            <div class="p-3 d-flex justify-content-end">
-                <button type="button" class="btn btn-success">Editar</button>
-                <button type="button" class="btn btn-danger ms-2">Excluir</button>
+            <div class="col-md-3 mb-5">
+                <div class="card h-100">
+                    <img src="{{ url("storage/$imageProduct->imagem") }}" class="card-img-top imgProduct" alt="...">
+                    <div class="p-3 d-flex justify-content-end">
+                        <div class="d-none AfterClick">
+                            <div class="d-flex d-flex justify-content-end">
+                                <input value="{{ old('imagem') }}" type="file"
+                                    class="form-control @error('imagem') is-invalid @enderror " id="imagem"
+                                    name="imagem[]" multiple>
+                                @error('imagem')
+                                    <p class="text-danger">{{ $message }}</p>
+                                @enderror
+                                <div><button type="button" class="btn btn-success ms-2">Editar</button></div>
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-success BeforeClick"
+                            onclick="ClickEdite()">Editar</button>
+                        <button type="button" class="btn btn-danger ms-2 BeforeClick">Excluir</button>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">Atualizado em {{ $imageProduct->atualizado }}</small>
+                    </div>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-body-secondary">Atualizado em 21/29/2023</small>
-            </div>
-            </div>
-        </div>
         @endforeach
     </div>
 
 </body>
-    @include('includes.scriptsbootstrap')
+@include('includes.scriptsbootstrap')   
+
 </html>
 
+<script>
+    function ClickEdite() {
+        const elements = $(".BeforeClick");
+        elements.attr('class', 'd-none');
 
-
-
+        const edit = $(".AfterClick");
+        edit.attr('class', 'd-block')
+    }
+</script>
