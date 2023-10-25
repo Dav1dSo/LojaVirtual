f6902daa4f43fb1d29ff8f97a872a9cb30508b2c
David <soaresdavid411@gmail.com>
2023-10-25 15:02:54 -0300

    BugFix: Resolved bug in edite Image.

--- database/migrations/2023_10_24_173914_view_images_products.php
+++ database/migrations/2023_10_24_173914_view_images_products.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,16 +12,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        "create or replace
-            algorithm = UNDEFINED view view_images_products as
-        select
-            p.id as id,
-            ip.products_id as products_id,
-            ip.path as imagem,
-            date_format(str_to_date(ip.updated_at, '%Y-%m-%d'), '%d/%m/%Y') as `atualizado`
-        from
-            images_products ip
-        join products p on p.id = ip.products_id;";
+        DB::statement(
+            "create or replace
+                algorithm = UNDEFINED view view_images_products as
+            select
+                ip.id as id,
+                ip.products_id as products_id,
+                ip.path as imagem,
+                date_format(str_to_date(ip.updated_at, '%Y-%m-%d'), '%d/%m/%Y') as `atualizado`
+            from
+                images_products ip
+            join products p on p.id = ip.products_id;"
+        );
     }
 
     /**
@@ -28,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('view_images_products');
+        Schema::statement('DROP VIEW IF EXISTS view_images_products');
     }
 };
--- database/migrations/2023_10_24_174508_view_products.php
+++ database/migrations/2023_10_24_174508_view_products.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,20 +12,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        "create or replace
-            algorithm = UNDEFINED view `view_products` as
-        select
-            p.id as id,
-            p.nome as nome,
-            p.valor as valor,
-            p.estoque as estoque,
-            p.descricao as descricao,
-            p`.categoria as categoria,
-            ip.path as imagem,
-            date_format(str_to_date(p.updated_at, '%Y-%m-%d'), '%d/%m/%Y') as `atualizado`
-        from
-            products p
-        join images_products ip on p.id = ip.products_id;";
+        DB::statement(
+            "create or replace
+                algorithm = UNDEFINED view `view_products` as
+            select
+                p.id as id,
+                p.nome as nome,
+                p.valor as valor,
+                p.estoque as estoque,
+                p.descricao as descricao,
+                p.categoria as categoria,
+                ip.path as imagem,
+                date_format(str_to_date(p.updated_at, '%Y-%m-%d'), '%d/%m/%Y') as `atualizado`
+            from
+                products p
+            join images_products ip on p.id = ip.products_id;"
+        );
     }
 
     /**
@@ -32,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('view_products');
+        Schema::statement('DROP VIEW IF EXISTS  view_products');
     }
 };
