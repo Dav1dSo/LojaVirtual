b522350bd6cfa95444d98c32b82f858577af5232
David <soaresdavid411@gmail.com>
2024-03-05 20:30:08 -0300

    feat: created controller for Articles.

--- app/Http/Controllers/Management/Articles/ArticlesController.php
+++ app/Http/Controllers/Management/Articles/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Management\Articles;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Management\ArticlesRequest;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
@@ -13,5 +14,16 @@ class ArticlesController extends Controller
 
     public function CreateArticle ()  {
         return view('management.ViewsArticles.formCreateArticle');
+    }
+
+    public function SaveArticle (ArticlesRequest $requestArticles) {
+
+        $dataArticle = [
+            'titulo' => $requestArticles->titulo,
+            'imagem' => $requestArticles->imagem,
+            'textContent' => $requestArticles->textContent
+        ];
+
+        return 'Salvo com sucesso!';
     }
 }
