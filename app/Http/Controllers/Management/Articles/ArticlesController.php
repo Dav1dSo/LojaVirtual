2bc4e0ad739926df9a1ed4e52d8f05501c2c2ca5
David <soaresdavid411@gmail.com>
2024-03-05 22:42:22 -0300

    feat: ajusted upload de imagem.

--- app/Http/Controllers/Management/Articles/ArticlesController.php
+++ app/Http/Controllers/Management/Articles/ArticlesController.php
@@ -4,25 +4,45 @@ namespace App\Http\Controllers\Management\Articles;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Management\ArticlesRequest;
+use App\Models\Articles;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    public function GetArticles () {
+    public function GetArticles()
+    {
         return view('management.ViewsArticles.Articles');
     }
 
-    public function CreateArticle ()  {
+    public function CreateArticle()
+    {
         return view('management.ViewsArticles.formCreateArticle');
     }
 
-    public function SaveArticle (ArticlesRequest $requestArticles) {
+    public function SaveArticle(ArticlesRequest $requestArticles)
+    {
+        $Article = new Articles();
+
+        $ImageArticle = $requestArticles->file('imagem');
+
+        $filepaths = [];
+
+        if (is_array($ImageArticle)) {
+            foreach ($ImageArticle as $fileImage) {
+                $filepath = $fileImage->store('img/Articles', 'public');
+                $filepaths[] = $filepath;
+            }
+        } else {
+            $filepaths = $ImageArticle->store('img/Articles', 'public');
+        }
 
         $dataArticle = [
             'titulo' => $requestArticles->titulo,
-            'imagem' => $requestArticles->imagem,
+            'imagem' => $filepaths,
             'textContent' => $requestArticles->textContent
         ];
+
+        $Article::create($dataArticle);
 
         return 'Salvo com sucesso!';
     }
